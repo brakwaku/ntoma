1e1d0383e40e55522a0b45204aa0d82cd97cfbf3
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-06-21 16:42:23 -0600

    Set up utils

---
+++ src/utils/firebase/firebase.utls.js
@@ -0,0 +1,54 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA_pATta8EhL4LcgMsvb7Dn-f6Jw1SCSv0',
+  authDomain: 'ntoma-db.firebaseapp.com',
+  projectId: 'ntoma-db',
+  storageBucket: 'ntoma-db.appspot.com',
+  messagingSenderId: '114819146373',
+  appId: '1:114819146373:web:d1bc508599a4186ed2bfec',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
