a3ced8ea197fcd28a56eac25145e0f837287bdfd
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-06-23 18:32:44 -0600

    fix typo in file name / sign in with email and password

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,7 @@ import {
   GoogleAuthProvider,
   // FacebookAuthProvider,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -63,4 +64,9 @@ export const createUserDocumentFromAuth = async (userAuth, additionalIformation 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if(!email || !password) return;
   return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
 }
