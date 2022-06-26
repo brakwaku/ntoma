d7d0531821630cae1aaa3c2f69b0d32525a8fe9e
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-06-26 00:33:50 -0600

    Code cleanup

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -4,9 +4,8 @@ import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.componet";
 
 import {
-    createUserDocumentFromAuth,
-    signInWithGooglePopup,
     signInAuthUserWithEmailAndPassword,
+    signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
 
 import './sign-in-form.styles.scss';
@@ -32,9 +31,7 @@ const SignInForm = () => {
         e.preventDefault();
 
         try {
-            const { user } = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(user)
-
+            await signInAuthUserWithEmailAndPassword(email, password);
             resetFormFields();
 
         } catch (error) {
@@ -82,7 +79,7 @@ const SignInForm = () => {
 
                 <div className="buttons-container">
                     <Button type="submit">Sign In</Button>
-                    <Button type='button' buttonType="google" onClick={signInWithGoogle}>Google sign In</Button>
+                    <Button type='button' buttonType="google" onClick={signInWithGoogle}>Sign In With Google</Button>
                 </div>
             </form>
         </div>
