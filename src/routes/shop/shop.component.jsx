fad7b8551dfa2791755b5d767d8614e8e1841ab8
brakwaku <brakwakujr@gmail.com>
2022-07-09 01:05:55 -0600

    Refactor nested routing

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,15 @@
-import { useContext } from 'react';
+import { Routes, Route } from "react-router-dom";
 
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import CategoriesPreview from "../categories-preview/categories-preview.component.";
 
-import './shop.styles.scss';
+import "./shop.styles.scss";
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-    return (
-        <div className='shop-container'>
-            {Object.keys(categoriesMap).map(title => {
-                const products = categoriesMap[title];
-                return <CategoryPreview key={title} title={title} products={products} />
-                })}
+  return (
+    <Routes>
+      <Route index element={ <CategoriesPreview /> } />
+    </Routes>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Shop
+export default Shop;
