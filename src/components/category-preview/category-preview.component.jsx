e6f2648630d06d7eeeb564c6a5c13cf49b5b7810
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-07-09 00:29:38 -0600

    Complete card preview

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,5 @@
+import ProductCard from '../product-card/product-card.component';
+
 import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
@@ -7,7 +9,10 @@ const CategoryPreview = ({ title, products }) => {
                 <span className='title'>{title.toUpperCase()}</span>
             </h2>
             <div className='preview'>
-                {}
+                {products
+                    .filter((_, idx) => idx < 4)
+                    .map((product) => (<ProductCard key={product.id} product={product} />
+                    ))}
             </div>
         </div>
     )
