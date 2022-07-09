39015310b34dae245ebe43fa4e2fbd411e9b9d1b
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-07-09 02:04:32 -0600

    Refactor component rename

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,15 @@
+import './directory-item.styles.scss';
+
+const DirectoryItem = ({ title, imageUrl }) => {
+    return (
+        <div className="directory-item-container">
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+            <div className="body">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default DirectoryItem;
